278383e68a3adc79849836a7c1873d108a4481dd
ChrisArreFall <chrisarrefall@gmail.com>
2019-04-20 19:33:09 -0600

    Update flags.sv

--- Processor/ALU/flags.sv
+++ Processor/ALU/flags.sv
@@ -3,8 +3,8 @@ module flags#(parameter N_b=32)(input logic [N_b-1:0] A,B,SUM,result,
 									input logic [3:0] ALUControl,
 									output logic [3:0] FLAGS);
 				//flags 3 2 1 0
-				//      Z N C V
-				logic Z,N,C,V;
+				//      N Z C V
+				logic N,Z,C,V;
 				assign V = ~(ALUControl[0] ^ A[N_b-1] ^ B[N_b-1]) & (A[N_b-1]^SUM[N_b-1]) & (~ALUControl[1]);
 				assign C = (~ALUControl[1]) & Cout;
 				assign N = result[N_b-1];
@@ -16,5 +16,5 @@ module flags#(parameter N_b=32)(input logic [N_b-1:0] A,B,SUM,result,
 						else
 								Z=0;
 					end
-				assign FLAGS = {Z,N,C,V};
+	assign FLAGS = {N,Z,C,V};
 endmodule
